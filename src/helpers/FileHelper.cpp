3da72a31f85da645079c612e7e2555726b63f06a
tnicolas28 <54882586+tnicolas28@users.noreply.github.com>
2022-02-27 22:50:46 +0100

    App ready

---
+++ src/helpers/FileHelper.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nicolas TRAN on 27/02/2022.
+//
+
+#include "FileHelper.h"
---
+++ src/helpers/FileHelper.h
@@ -0,0 +1,14 @@
+//
+// Created by Nicolas TRAN on 27/02/2022.
+//
+
+#ifndef SVG_CREATOR_FILEHELPER_H
+#define SVG_CREATOR_FILEHELPER_H
+
+
+class FileHelper {
+
+};
+
+
+#endif //SVG_CREATOR_FILEHELPER_H
